937c02ae743b5138ea9ebd2318e0ab5d66acbbb5
SinotovOleksii <142257730+SinotovOleksii@users.noreply.github.com>
2023-09-02 23:10:00 +0300

    Update todo.mjs

--- todo.mjs
+++ todo.mjs
@@ -20,7 +20,7 @@ for  (const [key, val] of Object.entries(expectedObj)){
 }
 
 console.log('b:', JSON.stringify(b));
-
+//from www
 //add from pc
 //create tests for gnfl and dont fuck this
 //fuck this if any
