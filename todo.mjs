eb195ff7829e6b0db1085387f35ccd49c3dac1f3
Alex Sinotov <sinotov.alex@gmail.com>
2023-09-03 11:30:45 +0300

    todo33

--- todo.mjs
+++ todo.mjs
@@ -20,7 +20,7 @@ for  (const [key, val] of Object.entries(expectedObj)){
 }
 
 console.log('b:', JSON.stringify(b));
-//from www22
+//from www2233
 //add from pc
 //create tests for gnfl and dont fuck this
 //fuck this if any
